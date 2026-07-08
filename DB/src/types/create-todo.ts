c76eecc29e71c49b3fa18cb4908ac9fa1c4b0336
fanil279 <fanilramakaev@gmail.com>
2026-07-08 20:28:33 +0500

    Remove DB package and SQL solution files; delete TypeORM entities, services, and related files; clear out Redis client and TypeScript configuration.

--- DB/src/types/create-todo.ts
+++
@@ -1,9 +0,0 @@
-import type { UserEntity } from '../entities/User';
-import type { Status } from '../entities/Todo';
-
-export interface CreateTodo {
-    title: string;
-    description: string;
-    status: Status;
-    userId: number;
-}
